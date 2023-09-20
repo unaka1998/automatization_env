e39f5112a2b329ccc58de00bb780886afe6a0903
unaka1998 <72253913+unaka1998@users.noreply.github.com>
2023-09-20 11:22:50 +0300

    Update test_all_issuers_v2.py

--- test_all_issuers_v2.py
+++ test_all_issuers_v2.py
@@ -23,10 +23,10 @@ ftnpwd='qwer123'
 # Path to text document containing card information
 # Format in .txt file:
 # card_PAN auth_method(otp/potp) password(optional, default=parole)
-test_card_path = "https://github.com/unaka1998/automatization_env/blob/main/all_cards.txt"
+test_card_path = "https://github.com/unaka1998/automatization_env/blob/37f2885919249a1c61c161ef6100085fcbf62d5b/all_cards.txt"
 
 # Path to test results (currently in same dir as the .py file)
-test_result_path = "https://github.com/unaka1998/automatization_env/tree/main/Result"
+test_result_path = "https://github.com/unaka1998/automatization_env/tree/37f2885919249a1c61c161ef6100085fcbf62d5b/Result"
 
 # Path to BankId app desktop version
 #bankIdAppPath = "C:\\Program Files (x86)\\BankID\\BankID.exe"
